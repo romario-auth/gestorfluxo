ec685ca19925e29c77d6187879f0644752c5db47
Romario Damasceno <romario.auth@gmail.com>
2026-05-13 18:35:58 -0300

    feat: white-label GestorFluxo logo

--- config/initializers/brand.rb
+++ config/initializers/brand.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  config.x.product_name = ENV.fetch("PRODUCT_NAME", "Sure")
-  config.x.brand_name = ENV.fetch("BRAND_NAME", "FOSS")
+  config.x.product_name = ENV.fetch("PRODUCT_NAME", "GestorFluxo")
+  config.x.brand_name = ENV.fetch("BRAND_NAME", "GestorFluxo")
 end
